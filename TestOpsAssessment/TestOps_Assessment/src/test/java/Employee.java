592ac9071042b2f4392b646b398354ddc6434583
Chima Durumetu <stanleychukwuma20@gmail.com>
2023-09-10 22:06:10 +0100

    Add files via upload
    TestOps Assessment

---
+++ TestOpsAssessment/TestOps_Assessment/src/test/java/Employee.java
@@ -0,0 +1,36 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class Employee {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+
+        // Create a new instance of the ChromeDriver
+        driver = new ChromeDriver();
+
+        // Navigate to the website
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+
+        // Maximize page
+        driver.manage().window().maximize();
+
+        // page to wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
